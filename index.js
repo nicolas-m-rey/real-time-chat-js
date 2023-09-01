104112807eb01561f9d10eadcdf1bcf8b99dfa85
nicolas-m-rey <nic.m.rey@gmail.com>
2023-09-01 14:43:26 -0700

    Fixed spacing as well as a type in stylesheet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
- 
+
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         fs.readFile(__dirname + '/index.html', (err, data) => {
@@ -14,20 +14,20 @@ const server = http.createServer((req, res) => {
         });
     }
 });
- 
+
 const io = require('socket.io')(server);
 const port = 5000;
- 
+
 io.on('connection', (socket) => {
     socket.on('send name', (user) => {
         io.emit('send name', user);
     });
- 
+
     socket.on('send message', (chat) => {
         io.emit('send message', chat);
     });
 });
- 
+
 server.listen(port, () => {
     console.log(`Server is listening at the port: ${port}`);
 });
